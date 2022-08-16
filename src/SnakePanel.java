ef41e0115c4336d28a472dad7fc66329db0fe47b
DarthPorky <mc.machika@gmail.com>
2022-08-16 15:41:11 +0200

    Source code for game frame, panel and logic.

---
+++ src/SnakePanel.java
@@ -0,0 +1,18 @@
+import javax.swing.JPanel;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SnakePanel extends JPanel implements ActionListener{
+	
+	SnakePanel(){
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
